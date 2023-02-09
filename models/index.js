34e1fed98637e8a0c7509145218a99fa06ddf6e7
agtTwilight <lukasmacmillen@gmail.com>
2023-02-09 11:46:46 -0800

    updated associations

--- models/index.js
+++ models/index.js
@@ -3,13 +3,12 @@
 const Achievement = require("./tavern/Achievement");
 const Comment = require("./tavern/Comment");
 const Group = require("./tavern/Group");
-const Journal = require("./tavern/Journal");
+const GroupTag = require("./tavern/GroupTag");
 const User = require("./tavern/User");
 const Character = require("./game/Character");
 const Forge = require("./game/Forge");
 const Item = require("./game/Item");
 const Spell = require("./game/Spell");
-const Achievement = require("./tavern/Achievement");
 
 // example association. Documentation link: https://sequelize.org/docs/v6/core-concepts/assocs/
 // TODO: add all associations shown in ERDs
@@ -27,7 +26,7 @@ Character.belongsTo(User,{
   User.hasMany(Character)
   
   // User associations with groups...can change to group hasmany users if we want users to only be able to join a single group.
-  Group.belongsToMany(User, {through})
+  Group.belongsToMany(User, {through: GroupTag})
 
 // TODO export all updated (associations added) models
 module.exports = {
