16200427edce1fb3da6e68632d37a13558620a91
Jason Nguyen <jhnwoo.dev@gmail.com>
2023-02-09 11:28:58 -0800

    updated models/taver/Achievement and Comment - small update to association in modes/index.js

--- models/index.js
+++ models/index.js
@@ -1,23 +1,26 @@
 // boiler plate
 // TODO: before adding model associations...create your models!
-const Achievements = require("./tavern/Achievement");
+const Achievement = require("./tavern/Achievement");
 const Comment = require("./tavern/Comment");
 const Group = require("./tavern/Group");
 const Journal = require("./tavern/Journal");
 const User = require("./tavern/User");
-const Character = require("./game/Character")
-const Forge = require("./game/Forge")
-const Item = require("./game/Item")
-const Spell = require("./game/Spell")
+const Character = require("./game/Character");
+const Forge = require("./game/Forge");
+const Item = require("./game/Item");
+const Spell = require("./game/Spell");
+const Achievement = require("./tavern/Achievement");
 
 // example association. Documentation link: https://sequelize.org/docs/v6/core-concepts/assocs/
 // TODO: add all associations shown in ERDs
-Character.belongsTo(User,{
-    onDelete:"CASCADE"
-})
-User.hasMany(Character)
+Comment.belongsTo(Achievement, {
+    onDelete: "CASCADE",
+});
+Achievement.hasMany(Comment);
 
 // TODO export all updated (associations added) models
 module.exports = {
     User,
-}
+    Comment,
+    Achievement,
+};
--- models/tavern/Comment.js
+++ models/tavern/Comment.js
@@ -1,20 +1,21 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 // Boiler plate sequelize model construction ex. taken from unfinished Character.js file
-// TODO: remove instance of character with the current files name
-class Character extends Model {}
+class Comment extends Model {}
 
-Character.init({
+Comment.init(
+    {
         // example data value
         // TODO: use the correct ERD in the resources tab to fill out model data structure
-    character_name: {
-         type: DataTypes.STRING,
-         allowNull:false,
-         unique: true
+        text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
     }
-},{
-    sequelize
-});
+);
 
-// export character
-module.exports= Character
+// export comment
+module.exports = Comment;
