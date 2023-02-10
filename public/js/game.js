66efd0fed36a4b5a716444952a5c973f0919d2d1
OneDeadFox <tjosephb.pro@gmail.com>
2023-02-10 10:58:36 -0800

    wip

--- public/js/game.js
+++ public/js/game.js
@@ -5,8 +5,27 @@ const actionMenuBtn4 = document.querySelector(`#am-btn-4`);
 const pauseBtn = document.querySelector(`.pause-btn`);
 const pauseMenu = document.querySelector(`#pause-menu`)
 
-
+console.log(pauseMenu.style.left);
 
 pauseBtn.addEventListener(`click`, function() {
-    const movePauseMenu = setInterval()
-})
+    console.log(`ouch`);
+    pauseMenu.setAttribute(`style`, `left: 0px; transition: .5s`)
+    //movePauseMenu();
+});
+
+const movePauseMenu = () => {
+    menuOffset = -230;
+    const moveing = setInterval(function () {
+        menuOffset += 5;
+        pauseMenu.style.left = menuOffset + `px`;
+        console.log(menuOffset);
+        console.log(menuOffset);
+
+        if(menuOffset <= 0){
+            clearInterval(moveing)
+        }
+
+    }, 1);
+
+    
+}
