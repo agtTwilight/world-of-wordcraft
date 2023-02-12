1d6bd6d4c06417a13460dfe19f7489155fdf9b23
OneDeadFox <tjosephb.pro@gmail.com>
2023-02-12 05:40:13 -0800

    wip

--- public/js/game.js
+++ public/js/game.js
@@ -4,15 +4,24 @@
 const actionMenu = document.querySelector(`#action-menu`);
 const actionMenuBtn1 = document.querySelector(`#am-btn-1`);
 const actionMenuBtn2 = document.querySelector(`#am-btn-2`);
-const inventoryBtn = document.querySelector(`#am-btn-3`);
-const spellbookBtn = document.querySelector(`#am-btn-4`);
+const actionMenuBtn3 = document.querySelector(`#am-btn-1`);
+const actionMenuBtn4 = document.querySelector(`#am-btn-2`);
+
+//Pause menu buttons
 const pauseBtn = document.querySelector(`.pause-btn`);
-const progressBtn = document.querySelector(`.progress-btn`);
 const pauseMenu = document.querySelector(`#pause-menu`)
 const dataStorage = document.querySelector("#data-storage")
 
 //variable buttons
 const continueBtn = document.querySelector(`button[subclass = 'continue']`);
+const attackBtn = document.querySelector(`button[subclass = 'attack']`);
+const inventoryBtn = document.querySelector(`button[subclass = 'inventory']`);
+const useBtn = document.querySelector(`button[subclass = 'use']`);
+const spellbookBtn = document.querySelector(`button[subclass = 'spellbook']`);
+const libraryBtn = document.querySelector(`button[subclass = 'library']`);
+const addBtn = document.querySelector(`button[subclass = 'add']`);
+const dropBtn = document.querySelector(`button[subclass = 'drop']`);
+const backBtn = document.querySelector(`button[subclass = 'back']`);
 
 //Imported Classes
 import * as Character from "./initCharacter.js";
@@ -27,12 +36,25 @@ const gameController = new Game;
 gameController.gameInit(1);
 gameController.storyboxInit(dataStorage);
 actionMenuBtn1.textContent = `Continue`;
-actionMenuBtn1.classList.add(`continue`);
+actionMenuBtn1.classList.add(`continue-action`);
 
 actionMenu.addEventListener(`click`, function(e) {
-    const target = e.target.closest(`.continue`);
+    const continueAct = e.target.closest(`.continue-action`);
+    const attackAct = e.target.closest(`.attack-action`);
+    const inventoryAct = e.target.closest(`.inventory-action`);
+    const useAct = e.target.closest(`.use-action`);
+    const libraryAct = e.target.closest(`.library-action`);
+    const spellbookAct = e.target.closest(`.spellbook-action`);
+    const addAct = e.target.closest(`.add-action`);
+    const dropAct = e.target.closest(`.drop-action`);
+    const backAct = e.target.closest(`.back-action`);
 
-    if(target) {
+
+    if(continueAct) {
+        gameController.progressStory(dataStorage);
+    } else if(continueAct) {
+        gameController.progressStory(dataStorage);
+    } else if(continueAct) {
         gameController.progressStory(dataStorage);
     }
 })
