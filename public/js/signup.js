2bb2c4b37bcc3541f8e063011f970b6ad424c374
agtTwilight <lukasmacmillen@gmail.com>
2023-02-13 13:20:06 -0800

    modified signup function to create characters for new users

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,21 +1,22 @@
-document.querySelector("#signup-form").addEventListener("submit",e=>{
+const createNewCharacter = require("../../game_assests/playableCharacters")
+
+document.querySelector("#signup-form").addEventListener("submit", async e=>{
         e.preventDefault();
         const signupObj = {
             username:document.querySelector("#signup-username").value,
             password:document.querySelector("#signup-password").value
         }
-        console.log(signupObj)
-        fetch("/api/users/",{
+
+        const rawUserData = await fetch("/api/users/",{
             method:"POST",
             body:JSON.stringify(signupObj),
             headers:{
                 "Content-Type":"application/json"
             }
-        }).then(res=>{
-            if(res.ok){
-               location.href="/home"
-            } else {
-                alert("trumpet sound")
-            }
         })
+        const newUser = rawUserData.json();
+        const character_name = document.querySelector("#signup-character-name")
+        
+        await createNewCharacter(character_name, newUser.id)
+        location.href="/home"
     })
