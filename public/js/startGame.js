ce9358a31f881e878db3e5550724cbc817492687
agtTwilight <lukasmacmillen@gmail.com>
2023-02-13 21:32:29 -0800

    fixed start game button, added some styling

--- public/js/startGame.js
+++ public/js/startGame.js
@@ -1,16 +1,3 @@
-const Game = require(`../../game_assests/Classes/Game`);
-const Enemy = require(`../../game_assests/Classes/Enemy`);
-
 document.querySelector("#game-btn").addEventListener("click",e=>{
-        e.preventDefault();
-
-        //a bit of a misnomer. I think Mob should be renamed to GameRun or just Run.
-        const newGame = new Game;
-
-
         location.href = "/game"
-    })
-
-    // TODO: On user signup:
-    // get character data templates (not from db)
-    // Create three new characters
+})
